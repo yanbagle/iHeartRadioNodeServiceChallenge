b974aa16c434abaf2635eda58b8749b51cc30cb7
yanzhang000 <circusburger63@yahoo.com>
2017-12-16 20:32:14 -0500

    fixed bug with url

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,22 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
 var request = require('request');
 
 export default ({ config, db }) => {
 	let api = Router();
 
-	var url = 'http://us.api.iheart.com/api/v1/catalog/searchAll?keywords=';
+	var url1 = 'http://us.api.iheart.com/api/v1/catalog/searchAll?keywords=';
 	var url2 = `&queryTrack=false&queryBundle=false&queryArtist=true&queryStation=false&queryFeaturedStation=false&queryTalkShow=false&queryTalkTheme=false&queryKeyword=false&countryCode=US`;
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
 
 	api.get('/search', (req, res) => {
+        res.setHeader('Content-Type', 'application/json');
         var keyword = req.param('keyword');
-        url += keyword + url2;
+        var url = url1 + keyword + url2;
         request(url, function (error, response, body) {
             if (response) {
                 res.send(body);
             } else {
-                console.log('error:', error); 
+                console.log('error:', error);
                 res.send(error);
             }
         });
